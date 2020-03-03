72af54d1e2e966a3d90c472908c6170ef9e33cbc
codershona <falgunimst95@gmail.com>
2020-03-03 20:07:48 +0100

    javascript question errors fix

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 function populate() {
-  
+  if(quiz.isEnded()) {
+    // showScores();
+  }
+  else {
+    // show // QUESTION:
+    var element = document.getElementById("question");
+    element.innerHTML = quiz.getQuestionIndex().text;
+  }
 }
 
 var questions = [
-  new Questions("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
-  new Questions("Which language is used for styling web pages?", ["HTML", "Jquery", "CSS", "XML"], "CSS"),
-  new Questions("There are ___ main components of object oriented programming.", ["1", "6", "2", "4"], "4"),
-  new Questions("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
-  new Questions("MVC is a ___.", ["Language", "Library", "Framework", "All"], "Framework")
+  new Question("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
+  new Question("Which language is used for styling web pages?", ["HTML", "Jquery", "CSS", "XML"], "CSS"),
+  new Question("There are ___ main components of object oriented programming.", ["1", "6", "2", "4"], "4"),
+  new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
+  new Question("MVC is a ___.", ["Language", "Library", "Framework", "All"], "Framework")
 ];
 
 var quiz = new Quiz(questions);
